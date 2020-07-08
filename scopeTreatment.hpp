c0178866fe9b4883c12b5580c2002c7e1e58b9a6
geovani-moc <geovani.moc@outlook.com>
2020-07-08 15:53:02 -0300

    pequenas correcoes

--- scopeTreatment.hpp
+++ scopeTreatment.hpp
@@ -17,8 +17,8 @@ bool declarationStart(vector<Token> &derivation, int position);
 bool assignmentStart(vector<Token> &derivation, int position);
 bool scopeEnd(vector<Token> &derivation, int position);
 
-void newScope(vector<map<string, void *>> symbolTable)
-int insertSymbolInTable(vector<map<string, void *>> symbolTable, vector<Token> &derivation)
+void newScope(vector<map<string, void *>> symbolTable);
+int insertSymbolInTable(vector<map<string, void *>> symbolTable, vector<Token> &derivation);
 void performOperation();
 
 #endif
