ce1b2111098f937fd2d133a71ddc3dfa79b2015a
geovani-moc <geovani.moc@outlook.com>
2020-07-08 15:46:51 -0300

    implementado diversas funcoes

--- scopeTreatment.cpp
+++ scopeTreatment.cpp
@@ -2,12 +2,12 @@
 
 void scopeTreatment(vector<vector<Token>> &parserTree)
 {
-    int root = (int)parserTree.size() -1;
-    vector<map<string, void*>> symbolTable;
+    int root = (int)parserTree.size() - 1;
+    vector<map<string, void *>> symbolTable;
     scopeTreatment(parserTree, root, symbolTable);
 }
 
-void scopeTreatment(vector<vector<Token>> &parserTree, int root, vector<map<string, void*>> &symbolTable)
+void scopeTreatment(vector<vector<Token>> &parserTree, int root, vector<map<string, void *>> &symbolTable)
 {
     Token token;
     vector<Token> derivation = parserTree[root];
@@ -19,21 +19,26 @@ void scopeTreatment(vector<vector<Token>> &parserTree, int root, vector<map<stri
         {
             int derivationPosition = stoi(token.symbol);
             scopeTreatment(parserTree, derivationPosition, symbolTable);
-        }
-        else if (scopeStart())
-        {
-            newScope();
-            position++;
-        }
-        else if(declarationStart())
-        {
-            allocateSymbol();
             return;
         }
-        else if(assignmentStart())
+        else if (declarationStart(derivation, position))
+        {
+            position = insertSymbolInTable(symbolTable, derivation);
+        }
+        else if (assignmentStart(derivation, position))
         {
             //falta mais coisas nesse
             performOperation();
+            return;
+        }
+        else if (scopeStart(derivation, position))
+        {
+            newScope(symbolTable);
+            position++;
+        }
+        else if (scopeEnd(derivation, position))
+        {
+            symbolTable.pop_back();
             return;
         }
         else
@@ -48,35 +53,60 @@ void scopeTreatment(vector<vector<Token>> &parserTree, int root, vector<map<stri
 
 bool endOfTreatment(vector<Token> &derivation, int position)
 {
-    return true;
+    int size = (int)derivation.size();
+    
+    return (position < size);
 }
 
-bool scopeStart()
+bool scopeStart(vector<Token> &derivation, int position)
 {
-    return true;
+    return derivation[position].symbol.compare("{") == 0
 }
 
-bool declarationStart()
+bool declarationStart(vector<Token> &derivation, int position)
 {
-    return true;
+    return ((derivation[position].symbol.compare("int") == 0) || 
+    (derivation[position].symbol.compare("long") == 0));
 }
 
-bool assignmentStart()
+bool assignmentStart(vector<Token> &derivation, int position)
 {
-    return true;
+    return derivation[position].symbol.compare("ID") == 0;
 }
 
-void newScope()
+void newScope(vector<map<string, void *>> symbolTable)
 {
-
+    map<string, void *> newSymbolTable;
+    newSymbolTable = symbolTable.back();
+    symbolTable.push_back(newSymbolTable);
 }
 
-void allocateSymbol()
+int insertSymbolInTable(vector<map<string, void *>> symbolTable, vector<Token> &derivation)
 {
+    int positionID;
+    if(derivation[0].symbol.compare("int") == 0)
+    {
+        positionID = 1;
+        symbolTable.back()[derivation[positionID].symbol] = malloc(sizeof(int));
+    }
+    else if(derivation[0].symbol.compare("long") == 0)
+    {
+        positionID = 3;
+        symbolTable.back()[derivation[positionID].symbol] = malloc(sizeof(long long int));
+    }
+    else
+    {
+        fprintf(stderr, "Erro: nao encontrado o tipo de simbolo.\n");
+        exit(EXIT_FAILURE);
+    }
 
+    return positionID+2; //checar overflow
 }
 
 void performOperation()
 {
+}
 
+bool scopeEnd()
+{
 }
--- scopeTreatment.hpp
+++ scopeTreatment.hpp
@@ -10,13 +10,15 @@ using namespace std;
 
 void scopeTreatment(vector<vector<Token>> &parserTree);
 void scopeTreatment(vector<vector<Token>> &parserTree, int root, vector<map<string, void*>> &symbolTable);
-bool endOfTreatment(vector<Token> &position);
 
-bool scopeStart();
-bool declarationStart();
-bool assignmentStart();
-void newScope();
-void allocateSymbol();
+bool endOfTreatment(vector<Token> &derivation, int position);
+bool scopeStart(vector<Token> &derivation, int position);
+bool declarationStart(vector<Token> &derivation, int position);
+bool assignmentStart(vector<Token> &derivation, int position);
+bool scopeEnd(vector<Token> &derivation, int position);
+
+void newScope(vector<map<string, void *>> symbolTable)
+int insertSymbolInTable(vector<map<string, void *>> symbolTable, vector<Token> &derivation)
 void performOperation();
 
 #endif
